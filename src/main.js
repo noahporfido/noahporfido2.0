3a93ff434cc1e73cb22274740e8eb0e4b36145bb
Noah Porfido <noahporfido@Noahs-MacBook-Pro-2.local>
2020-10-14 10:55:14 +0200

    added fullpage vue

--- src/main.js
+++ src/main.js
@@ -4,7 +4,13 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
+// import 'fullpage.js/vendors/scrolloverflow' // Optional. When using scrollOverflow:true
+// import './fullpage.scrollHorizontally.min' // Optional. When using fullpage extensions
+import VueFullPage from 'vue-fullpage.js'
+
 Vue.config.productionTip = false
+
+Vue.use(VueFullPage);
 
 new Vue({
   router,
