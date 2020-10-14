607f6c53eaecbbbd12c565cdc6cd3691e0ef219a
Noah Porfido <noahporfido@noahs-macbook-pro.home>
2020-10-14 19:44:49 +0200

    changed varibales, aboutpage start

---
+++ src/components/sections/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about-section">
+    <div class="image-top"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.about-section {
+  //   .image-top {
+  //     background-size: cover;
+  //     background-position: 42%;
+  //     height: 500px;
+  //     background-repeat: no-repeat;
+  //   }
+}
+</style>
