5167bb4a0c04db8854e221af9aab4d21737c3501
Noah Porfido <noah.porfido@icloud.com>
2021-04-22 20:41:36 +0200

    added antdesign

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+import './assets/styles/reset.css'
 // import 'fullpage.js/vendors/scrolloverflow' // Optional. When using scrollOverflow:true
 // import './fullpage.scrollHorizontally.min' // Optional. When using fullpage extensions
 import VueFullPage from 'vue-fullpage.js'
@@ -11,6 +13,8 @@ import VueFullPage from 'vue-fullpage.js'
 Vue.config.productionTip = false
 
 Vue.use(VueFullPage);
+
+Vue.use(Antd);
 
 new Vue({
   router,
