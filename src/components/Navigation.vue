bd057f09779f0f80816c66623c0297a0cb5a7608
Noah Porfido <noah.porfido@icloud.com>
2021-05-02 11:31:02 +0200

    nav done

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -4,9 +4,10 @@
     class="navigation-container"
   >
     <ul>
-      <li @click="scrollToSection('#home')">home</li>
-      <li @click="scrollToSection('#about')">about me</li>
-      <li @click="scrollToSection('#projects')">projects</li>
+      <li class="bold rotate90" @click="scrollToSection('#home')">N</li>
+      <li @click="scrollToSection('#values')">Werte</li>
+      <li @click="scrollToSection('#timeline')">Werdegang</li>
+      <li @click="scrollToSection('#skills')">Skills</li>
     </ul>
   </div>
 </template>
@@ -15,7 +16,6 @@
 export default {
   methods: {
     scrollToSection(hash) {
-      console.log(this.$route.hash);
       location.href = hash;
     },
   },
@@ -26,51 +26,64 @@ export default {
 @import "../assets/styles/_general.scss";
 
 .navigation-container {
-  z-index: 2;
-  position: fixed;
-  width: 100%;
-  padding: $md-pd 4%;
   ul {
     display: flex;
-    justify-content: flex-end;
-    list-style-type: none;
-
+    justify-content: space-evenly;
+    align-items: center;
+    height: 80px;
     li {
-      font-weight: 700;
       cursor: pointer;
-      padding: 0 $md-pd;
-      font-size: $title-md;
-      transition: all 0.5s ease;
-      animation: 1s slideIn;
-      position: relative;
-
-      &::after {
-        position: absolute;
-        content: "";
-        top: 100%;
-        left: 0;
-        width: 100%;
-        height: 3px;
-        background: #756cff;
-        transform: scaleX(0);
-        transform-origin: right;
-        transition: transform 0.5s;
-      }
-
-      &:hover::after {
-        transform: scaleX(1);
-        transform-origin: left;
-      }
-
-      &:nth-of-type(2) {
-        animation-duration: 1.3s;
-      }
-      &:nth-of-type(3) {
-        animation-duration: 1.6s;
-      }
+      font-size: $title-sm;
     }
   }
 }
+
+// .navigation-container {
+//   z-index: 2;
+//   position: fixed;
+//   width: 100%;
+//   padding: $lg-pd 4%;
+//   ul {
+//     display: flex;
+//     justify-content: flex-end;
+//     list-style-type: none;
+
+//     li {
+//       font-weight: 700;
+//       cursor: pointer;
+//       padding: 0 $md-pd;
+//       font-size: $title-sm;
+//       transition: all 0.5s ease;
+//       animation: 1s slideIn;
+//       position: relative;
+
+//       &::after {
+//         position: absolute;
+//         content: "";
+//         top: 100%;
+//         left: 0;
+//         width: 100%;
+//         height: 2px;
+//         background: black;
+//         transform: scaleX(0);
+//         transform-origin: right;
+//         transition: transform 0.5s;
+//       }
+
+//       &:hover::after {
+//         transform: scaleX(1);
+//         transform-origin: left;
+//       }
+
+//       &:nth-of-type(2) {
+//         animation-duration: 1.3s;
+//       }
+//       &:nth-of-type(3) {
+//         animation-duration: 1.6s;
+//       }
+//     }
+//   }
+// }
 
 @keyframes slideIn {
   0% {
